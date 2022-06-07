b595272ec8e562a1dc7093814733f81aaa58153b
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-07 01:30:12 -0300

    Usa o registrosRoutes no app.

--- back-end/api/app.js
+++ back-end/api/app.js
@@ -1,6 +1,9 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
+const { registrosRoutes } = require('../routes');
 
 app.use(bodyParser.json());
+
+app.use(registrosRoutes);
 
 module.exports = app;
