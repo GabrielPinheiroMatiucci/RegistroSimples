c6839c2ab958efe31e8d16cf21fc002de61e0950
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-07 21:23:25 -0300

    Adiciona um arquivo para os statusHTTP.

---
+++ back-end/utils/index.js
@@ -0,0 +1,9 @@
+const statusCode = require('./statusCode');
+const generateMessageJSON = require('./generateMessageJSON');
+const registrosQueries = require('./registrosQueries');
+
+module.exports = {
+  statusCode,
+  generateMessageJSON,
+  registrosQueries,
+};
