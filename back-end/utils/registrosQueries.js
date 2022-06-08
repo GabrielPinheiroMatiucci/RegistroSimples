6315e0d926e2b681bda61005a916dbe36fc99b51
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-08 18:04:12 -0300

    Atualiza a querie de consulta dos registros.

--- back-end/utils/registrosQueries.js
+++ back-end/utils/registrosQueries.js
@@ -1,4 +1,4 @@
-const getRegistros = 'SELECT * FROM tb_registros ORDER BY dt_nascimento ASC, cd_registro ASC;';
+const getRegistros = 'SELECT * FROM tb_registros ORDER BY dt_nascimento DESC, cd_registro ASC;';
 const createRegistro = 'INSERT INTO tb_registros ' +
   '(nm_pessoa, dt_nascimento) VALUES (?, ?);';
 
