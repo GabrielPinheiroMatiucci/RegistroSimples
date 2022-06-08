d6fb8a7f56f9f6fbf9ba594aa90a7328f7fd92f8
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-07 22:53:26 -0300

    Adiciona o AUTO_INCREMENT na PK.

--- database.sql
+++ database.sql
@@ -5,7 +5,7 @@ CREATE DATABASE db_registros;
 USE db_registros;
 
 CREATE TABLE tb_registros (
-  cd_registro INT,
+  cd_registro INT AUTO_INCREMENT,
   nm_pessoa VARCHAR(200) NOT NULL,
   dt_nascimento DATE NOT NULL,
   CONSTRAINT pk_registros
