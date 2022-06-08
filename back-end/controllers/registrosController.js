5500ba7f5b96024125068e207ea75df4301cad2e
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-07 21:28:26 -0300

    Adiciona o getRegistros().

--- back-end/controllers/registrosController.js
+++ back-end/controllers/registrosController.js
@@ -1,5 +1,16 @@
-function getRegistros(_req, res) {
+const { registrosService } = require('../services');
+const { statusCode, generateMessageJSON } = require('../utils');
 
+async function getRegistros(_req, res) {
+  try {
+    const registros = await registrosService.getRegistros();
+
+    return res.status(statusCode.ok).json(registros);
+  } catch(er) {
+    return res.status(
+      statusCode.internalServerError,
+    ).json(generateMessageJSON("Internal Server Error"));
+  }
 }
 
 module.exports = {
