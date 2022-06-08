7bb1d78c2788c1b838bdc08bf18b55ee0b6f9cdc
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-07 22:57:11 -0300

    Adiciona no controller e no service a createRegistro.

--- back-end/controllers/registrosController.js
+++ back-end/controllers/registrosController.js
@@ -6,13 +6,34 @@ async function getRegistros(_req, res) {
     const registros = await registrosService.getRegistros();
 
     return res.status(statusCode.ok).json(registros);
-  } catch(er) {
+  } catch(_er) {
     return res.status(
       statusCode.internalServerError,
-    ).json(generateMessageJSON("Internal Server Error"));
+    ).json(generateMessageJSON('Internal Server Error'));
+  }
+}
+
+async function createRegistro(req, res) {
+  const { name, date } = req.body;
+
+  try {
+    const result = await registrosService.createRegistro({ name, date });
+
+    if (result === null) {
+      return res.status(statusCode.badRequest).json(
+        generateMessageJSON('Invalid Body'),
+      );
+    }
+
+    return res.status(statusCode.created).end();
+  } catch(_er) {
+    return res.status(statusCode.internalServerError).json(
+      generateMessageJSON('Internal Server Error'),
+    );
   }
 }
 
 module.exports = {
   getRegistros,
+  createRegistro,
 };
