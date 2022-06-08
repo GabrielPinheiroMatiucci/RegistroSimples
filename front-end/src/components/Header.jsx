72a47308f52a30e73063e406291987ea6b0d772d
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-08 17:39:17 -0300

    Adiciona o componente Header.

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import Select from './Select';
+
+function Header() {
+  const now = new Date();
+  const [payload, setPayload] = useState({
+    day: `${now.getDate()}`,
+    month: `${(now.getMonth() + 1)}`,
+    year: `${now.getFullYear()}`,
+    fullName: '',
+  });
+
+  function handleChange({ target: { name, value } }) {
+    const oldPayload = payload;
+    setPayload({ ...oldPayload, [name]: value })
+  }
+
+  return (
+    <header className="header">
+      <h3>Inserir novo Registro</h3>
+      <nav className="navbar">
+        <label htmlFor="fullName" className="margin-right">
+          Nome:&nbsp;
+          <input type="text" name="fullName" onChange={ handleChange } />
+        </label>
+        <p>Data de nascimento:&nbsp;&nbsp;</p>
+        <Select
+          name="day"
+          value={ payload.day }
+          handleChange={ handleChange }
+          initial={ 1 }
+          limit={ 31 }
+        />
+        <Select
+          name="month"
+          value={ payload.month }
+          handleChange={ handleChange }
+          initial={ 1 }
+          limit={ 12 }
+        />
+        <Select
+          name="year"
+          value={ payload.year }
+          handleChange={ handleChange }
+          initial={ (Number(payload.year) - 100) }
+          limit={ Number(payload.year) }
+        />
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
