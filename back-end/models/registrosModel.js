5e726c52c9c65d17f0a2fa73f79a34c095c57d14
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-07 22:54:53 -0300

    Adiciona a createRegistro no model.

--- back-end/models/registrosModel.js
+++ back-end/models/registrosModel.js
@@ -4,9 +4,21 @@ const { registrosQueries } = require('../utils');
 async function getRegistros() {
   const response = await connection.execute(registrosQueries.getRegistros);
 
-  return response;
+  return response[0];
+}
+
+async function createRegistro({ name, date }) {
+  const response = await connection.execute(
+    registrosQueries.createRegistro,
+    [name, date],
+  );
+
+  if (response[0].affectedRows === 1) return true;
+
+  return null;
 }
 
 module.exports = {
   getRegistros,
+  createRegistro,
 };
