424d39d21c207cc55278fb5dbe15ab15512c3877
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-07 22:55:59 -0300

    Adiciona a rota POST /registros.

--- back-end/routes/registrosRoutes.js
+++ back-end/routes/registrosRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const { registrosController } = require('../controllers');
 
 router.get('/registros', registrosController.getRegistros);
+router.post('/registros', registrosController.createRegistro);
 
 module.exports = router;
