be3959d04f325d620aec20f95d42246fdc8e1cbd
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-08 18:18:51 -0300

    Adiciona a requisicao a API e mostra os registros.

--- front-end/src/components/RecordsContainer.jsx
+++ front-end/src/components/RecordsContainer.jsx
@@ -1,8 +1,47 @@
+import { useState, useEffect } from 'react';
+import Loading from './Loading';
+
 function RecordsContainer() {
+  const [loading, setLoading] = useState(true);
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    async function fetchRecords() {
+      const response = await fetch('http://127.0.0.1:3001/registros');
+      const json = await response.json();
+
+      setRecords(json);
+      setLoading(!loading);
+    }
+
+    fetchRecords();
+  }, []);
+
+  function renderRecords() {
+    return (
+      <>
+        { records.map(( { nm_pessoa: fullName, dt_nascimento: fullDate } ) => {
+          const originalDate = fullDate.split('T')[0];
+          const splitted = originalDate.split('-');
+          const birthDate = `${splitted[2]}/${splitted[1]}/${splitted[0]}`;
+
+          return (
+            <section>
+              <p>Nome: { fullName } </p>
+              <p>Data de nascimento: { birthDate } </p>
+            </section>
+          );
+        }) }
+      </>
+    );
+  }
+
   return (
-    <nav className="navbar">
-      <h1>Registros</h1>
-    </nav>
+    <main className="main">
+      <h2>Registros</h2>
+      { loading && <Loading /> }
+      { !loading && renderRecords() }
+    </main>
   );
 }
 
