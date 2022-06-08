a07d258f4631db3dbed660bc9be99356a6b79de4
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-07 22:55:22 -0300

    Adiciona uma funcao de validacao do body.

---
+++ back-end/utils/validateBody.js
@@ -0,0 +1,31 @@
+function validateDateFormat({ year, month, day }) {
+  if (year.length !== 4) return null;
+
+  if (month.length !== 2) return null;
+
+  if (day.length !== 2) return null;
+
+  return true;
+}
+
+function validateDate(date) {
+  const [year, month, day] = date.split('-', 3);
+
+  if (year === undefined || year.trim() === '') return null;
+
+  if (month === undefined || month.trim() === '') return null;
+
+  if (day === undefined || day.trim() === '') return null;
+
+  return validateDateFormat({ year, month, day });
+}
+
+module.exports = ({ name, date }) => {
+  if (name === undefined || date === undefined) return null;
+
+  if (name.trim() === '' || date.trim() === '') return null;
+
+  if (validateDate(date) === null) return null;
+
+  return true;
+};
