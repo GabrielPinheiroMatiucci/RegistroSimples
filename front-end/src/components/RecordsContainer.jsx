ca7a5433b0056de12617a8ce80c4dfa930b3e35c
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-09 00:21:09 -0300

    Adiciona o estado para poder recarregar os registros quando houver um POST.

--- front-end/src/components/RecordsContainer.jsx
+++ front-end/src/components/RecordsContainer.jsx
@@ -3,31 +3,31 @@ import Loading from './Loading';
 import Record from './Record';
 import ServidorError from './ServidorError';
 
-function RecordsContainer() {
+function RecordsContainer({ posted }) {
   const [loading, setLoading] = useState(true);
   const [records, setRecords] = useState([]);
   const [err, setErr] = useState(false);
 
-  useEffect(() => {
-    async function fetchRecords() {
-      try {
-        const response = await fetch('http://127.0.0.1:3001/registros');
-        if (response.status === 200) {
-          const json = await response.json();
-    
-          setErr(false);
-          setRecords(json);
-          setLoading(false);
-        }        
-      } catch (error) {
-        setErr(true);
-        setLoading(true);
-        console.error(error);
-      }
-    }
+  useEffect(() => { fetchRecords() }, []);
 
-    fetchRecords();
-  }, []);
+  useEffect(() => { fetchRecords() }, [posted]);
+
+  async function fetchRecords() {
+    try {
+      const response = await fetch('http://127.0.0.1:3001/registros');
+      if (response.status === 200) {
+        const json = await response.json();
+  
+        setErr(false);
+        setRecords(json);
+        setLoading(false);
+      }        
+    } catch (error) {
+      setErr(true);
+      setLoading(true);
+      console.error(error);
+    }
+  }
 
   function renderRecords() {
     return (
