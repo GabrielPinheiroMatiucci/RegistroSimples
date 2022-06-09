2bd216aff14d67b6cc26cd7fb4fe3104537d359f
Semnome1505 <Gabriel_1505@outlook.com>
2022-06-09 00:20:25 -0300

    Adiciona um estado para poder recarregar os registros quando houver um POST.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import { Header, RecordsContainer } from './components';
 import './App.css';
 
 function App() {
+  const [posted, setPosted] = useState(false);
   return (
     <>
-      <Header />
-      <RecordsContainer />
+      <Header setPosted={ setPosted } posted={ posted } />
+      <RecordsContainer posted={ posted } />
     </>
   );
 }
